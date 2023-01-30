e824e4074fb56254fb590c4b72859113c77294cc
Rafael Pilumyan <Rafael.pilumyan@gmail.com>
2023-01-30 20:10:31 +0200

    update: filtering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = ({ sortBtn }) => {
+const Header = ({ sortBtn, filterByArea, filterByRegion }) => {
   return (
     <div className="flex flex-col items-center mx-10">
       <div className="w-full text-center md:text-left text-4xl py-4 px-6 bg-gray-200">
@@ -14,12 +14,18 @@ const Header = ({ sortBtn }) => {
           >
             Sort by name
           </button>
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-full mb-4 md:mb-0">
-            button 2
+          <button
+            onClick={filterByArea}
+            className="bg-blue-500 text-white py-2 px-4 rounded-full mb-4 md:mb-0"
+          >
+            Filter by area
           </button>
         </div>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-full">
-          Button 3
+        <button
+          onClick={filterByRegion}
+          className="bg-blue-500 text-white py-2 px-4 rounded-full"
+        >
+          Filter by region "Oceania"
         </button>
       </div>
     </div>
